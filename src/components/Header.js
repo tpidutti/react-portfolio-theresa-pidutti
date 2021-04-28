7c525d613b35e10d3b49d7f1c4ca0ffb69332668
Theresa Pidutti <treepidutti@gmail.com>
2021-04-28 16:26:12 -0600

    update links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,13 @@ function Header() {
     <div className="jumbotron jumbotron-fluid">
       <div className="container-top">
         <h2>Theresa Pidutti</h2>
-        <img
-          id="header-photo"
-          src="./images/artLady 2.jpeg"
-          className="top-image"
-          alt="me"
-        />
-        <p>
-          <Link to="/">My Experience</Link>
-          <Link to="/">Contact Me</Link>
-          <Link to="/about-me">About Me</Link>
-          <Link to="/">Home</Link>
-        </p>
+        <img id="header-photo" src="./images/artLady 2.jpeg" className="top-image" alt="me"/>
+        <ul>
+        <li><Link to="/about-me">About Me</Link></li>
+          <li><Link to="/">My Experience</Link></li>
+          <li><Link to="/">Contact Me</Link></li>
+          <li><Link to="/">Home</Link></li>
+        </ul>
       </div>
     </div>
   );
