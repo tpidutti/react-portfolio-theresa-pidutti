15419ff4e4e22d2afe8198f04c37b328dfb58383
Theresa Pidutti <treepidutti@gmail.com>
2021-04-28 15:32:21 -0600

    update routes

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -5,7 +5,6 @@ function Aboutme (){
   <p className="my-paragraph">
   I am a psych nurse with ten years of experience.  I have an MA in Communication Studies and an MSN with a focus on nursing informatics.  I have worked in the behavioral health section of the emergency department; on locked, adult acute care units from 10 to 29 patients.  I have also worked in the emplyee health/infection control department which required keeping employee files up-to-date, as well as administering vaccinations, conducting mask fit tests, and conducting TB tests. 
 </p>
-
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container-top">
         <h2>Theresa Pidutti</h2>
-        <img id="header-photo" src="./images/artLady 2.jpeg" className="top-image" alt="me" />
+        <img
+          id="header-photo"
+          src="./images/artLady 2.jpeg"
+          className="top-image"
+          alt="me"
+        />
         <p>
-          <a className="connect" href="#my experience">
-            My Experience
-          </a>
-          <a className="connect" href="#contact me">
-            Contact Me
-          </a>
+          <Link to="/">My Experience</Link>
+          <Link to="/">Contact Me</Link>
+          <Link to="/about-me">About Me</Link>
+          <Link to="/">Home</Link>
         </p>
       </div>
     </div>
