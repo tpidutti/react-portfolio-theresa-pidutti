02c302dd6ca1b12858edae76466b1ed47c826f39
Theresa Pidutti <treepidutti@gmail.com>
2021-04-29 07:11:14 -0600

    update about me

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -4,6 +4,7 @@ function Aboutme (){
   return (
   <p className="my-paragraph">
   I am a psych nurse with ten years of experience.  I have an MA in Communication Studies and an MSN with a focus on nursing informatics.  I have worked in the behavioral health section of the emergency department; on locked, adult acute care units from 10 to 29 patients.  I have also worked in the emplyee health/infection control department which required keeping employee files up-to-date, as well as administering vaccinations, conducting mask fit tests, and conducting TB tests. 
+  I enter the full stack coding bootcamp for personal growth and had to look everything up because it was all a foreign language to me.  This portfolio shows how much I've learned and my progress in the last three months.  
 </p>
   )
 }
